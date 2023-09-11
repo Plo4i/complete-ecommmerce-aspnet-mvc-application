a655a3c8bace6fd4c614d501e7f754da0d3aa999
Plo4i <midag@abv.bg>
2023-09-11 16:56:56 +0200

    Push 2

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -9,5 +9,8 @@ namespace eTickets.Models
         public string ProfilePuctureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        // Relationships
+        public List<Movie> Movies { get; set;}
     }
 }
